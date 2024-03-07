b36512bad192e5a752390bb9cde764b0112f2b61
ComingS00n <gamesxbow@gmail.com>
2024-03-07 18:07:58 +0700

    up

--- faceDetectionOOP.py
+++ faceDetectionOOP.py
@@ -1,7 +1,4 @@
-import argparse
-import os
 import platform
-import sys
 from pathlib import Path
 
 import torch
@@ -10,21 +7,16 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 from yeelight import Bulb, discover_bulbs
+from collections import Counter
 import time
+import sys
 
-import threading
-
-from ultralytics.utils.plotting import Annotator, colors, save_one_box
+from ultralytics.utils.plotting import Annotator, colors
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2, increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh
+from utils.general import LOGGER, Profile, check_file, check_img_size, check_imshow,   cv2,  non_max_suppression,  scale_boxes, strip_optimizer
 from utils.torch_utils import select_device, smart_inference_mode
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 class FaceDetector:
     def __init__(self, weights, source, data, imgsz, conf_thres, iou_thres, max_det, device, classes, agnostic_nms, augment, visualize, update, line_thickness, hide_labels, hide_conf, half, dnn, vid_stride):
         self.weights = weights
@@ -49,7 +41,6 @@ class FaceDetector:
         self.half = half
         self.dnn = dnn
         self.vid_stride = vid_stride
-        # self.stop_webcam = False
 
     @smart_inference_mode()
     def run(self):
@@ -81,12 +72,7 @@ class FaceDetector:
         model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))
         seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
 
-        # start_time = time.time()
-        # duration = 3
         for path, im, im0s, vid_cap, s in dataset:
-            # if self.stop_webcam or time.time() - start_time >= duration:
-            #     self.cap.release()
-            #     break
             with dt[0]:
                 im = torch.from_numpy(im).to(model.device)
                 im = im.half() if model.fp16 else im.float()
@@ -125,11 +111,7 @@ class FaceDetector:
                         if names[int(c)] == 'visitor':
                             print("Visitor detected! Take appropriate action.")
                         else:
-                            # self.stop_webcam = True
-                            emotion_detector = EmotionDetector()
-                            emotion_detector.run(duration=3)
-                            emotion_detector.process_emotion_array()
-                            # break
+                            break
 
                         if save_img or self.save_crop or self.view_img:
                             c = int(cls)
@@ -146,6 +128,8 @@ class FaceDetector:
                     cv2.waitKey(1)
 
             LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+            if 'visitor' not in s:
+                break
         cv2.destroyAllWindows()
 
         t = tuple(x.t / seen * 1E3 for x in dt)
@@ -153,15 +137,18 @@ class FaceDetector:
         if self.update:
             strip_optimizer(self.weights[0])
 
+        # Start EmotionDetector
+        emotion_detector = EmotionDetector()
+        emotion_detector.run()
+
 class EmotionDetector:
-    max_emotion_array = []
 
     def __init__(self):
         self.emotion_dict = {0: "Angry", 1: "Happy", 2: "Neutral", 3: "Sad", 4: "Surprised"}
         self.load_emotion_model()
         self.cap = cv2.VideoCapture(0)
         self.bulb = Bulb("192.168.1.119")  # Replace with your bulb's IP address
-        self.emotion_array_list = []
+        self.emotion_array = []
         self.start_time = time.time()
     
     def load_emotion_model(self):
@@ -183,44 +170,43 @@ class EmotionDetector:
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
             emotion_prediction = self.emotion_model.predict(cropped_img)
             maxindex = int(np.argmax(emotion_prediction))
-            self.emotion_array_list.append(emotion_prediction[0].tolist())
+            self.emotion_array.append(self.emotion_dict[maxindex])
 
             elapsed_time = time.time() - self.start_time
 
             if elapsed_time >= duration:
-                max_emotion_array = max(self.emotion_array_list, key=lambda x: x[0])
-                maxindex = max_emotion_array.index(max(max_emotion_array))
-
                 # ทำงานกับ max_emotion_array ตามที่คุณต้องการ
-                self.process_emotion_array(max_emotion_array)
+                self.process_emotion_array()
 
                 # รีเซ็ตค่า emotion_array_list และ start_time เพื่อให้เริ่มนับใหม่ในรอบถัดไป
-                self.emotion_array_list = []
+                self.emotion_array = []
+
                 self.start_time = time.time()
             cv2.putText(frame, self.emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
+            print(self.emotion_array)
         return frame
     
-    def process_emotion_array(self, max_emotion_array):
-        maxindex = max_emotion_array.index(max(max_emotion_array))
-        emotion_label = self.emotion_dict[maxindex]
-        if emotion_label == "Angry":
+    def process_emotion_array(self):
+        emotion_counter = Counter(self.emotion_array)
+        most_common_emotion = emotion_counter.most_common(1)[0][0]
+        print(f"The most common emotion is: {most_common_emotion}")
+        if most_common_emotion == "Angry":
             self.set_bulb_color(41, 197, 246)  # Blue
             print("open blue light #29C5F6")
-        elif emotion_label == "Happy":
+        elif most_common_emotion == "Happy":
             self.set_bulb_color(230, 126, 34)  # Orange
             print("open orange light #E67E22")
-        elif emotion_label == "Neutral":
+        elif most_common_emotion == "Neutral":
             self.bulb.turn_off()
             print("off light")
-        elif emotion_label == "Sad":
+        elif most_common_emotion == "Sad":
             self.set_bulb_color(39, 174, 96)  # Green
             print("open green light #27AE60")
-        elif emotion_label == "Surprised":
+        elif most_common_emotion == "Surprised":
             self.set_bulb_color(244, 208, 63)  # Yellow
             print("open yellow light #F4D03F")
         else:
-            print(f"Unknown emotion label: {emotion_label}")
+            print(f"Unknown emotion label: {most_common_emotion}")
 
     def set_bulb_color(self, r, g, b):
         print(f"Setting bulb color: R={r}, G={g}, B={b}")
@@ -242,44 +228,3 @@ class EmotionDetector:
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        self.cap.release()
-        max_emotion_array = max(self.emotion_array_list, key=lambda x: x[0])
-        self.process_emotion_array(max_emotion_array)
-
-    
-
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'vMembers_v2.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob/screen/0(webcam)')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1
-    print_args(vars(opt))
-    return opt
-
-def main(opt):
-    check_requirements(ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
-    faceDetector = FaceDetector(opt.weights, opt.source, opt.data, opt.imgsz, opt.conf_thres, opt.iou_thres, opt.max_det, opt.device, opt.classes, opt.agnostic_nms, opt.augment, opt.visualize, opt.update, opt.line_thickness, opt.hide_labels, opt.hide_conf, opt.half, opt.dnn, opt.vid_stride)
-    faceDetector.run()
-
-if __name__ == '__main__':
-    opt = parse_opt()
-    main(opt)
--- run_test.py
+++ run_test.py
@@ -1,26 +1,52 @@
-from yeelight import Bulb, discover_bulbs
+import argparse
+from utils.general import  check_requirements,  print_args
 
-# Discover available Yeelight bulbs on the network
-bulbs = discover_bulbs()
+from faceDetectionOOP import FaceDetector, EmotionDetector
+from pathlib import Path
 
-# Print information about discovered bulbs
-for bulb in bulbs:
-    print("ID: {0}, IP: {1}".format(bulb["id"], bulb["ip"]))
+import sys
+import os
 
-# Connect to a specific Yeelight bulb
-bulb_ip = "192.168.1.119"  # Replace with the actual IP address of your Yeelight bulb
-bulb = Bulb(bulb_ip)
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-# Turn on the bulb
-bulb.turn_on()
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'vMembers_v2.pt', help='model path or triton URL')
+    parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1
+    print_args(vars(opt))
+    return opt
 
-# Change the color temperature (optional)
-bulb.set_color_temp(4000)
+def main(opt):
+    check_requirements(ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+    faceDetector = FaceDetector(opt.weights, opt.source, opt.data, opt.imgsz, opt.conf_thres, opt.iou_thres, opt.max_det, opt.device, opt.classes, opt.agnostic_nms, opt.augment, opt.visualize, opt.update, opt.line_thickness, opt.hide_labels, opt.hide_conf, opt.half, opt.dnn, opt.vid_stride)
+    faceDetector.run()
+    # emotionDetector = EmotionDetector()
+    # emotionDetector.run()
 
-# Change the brightness (optional)
-bulb.set_brightness(50)
-
-# Turn off the bulb after a delay (optional)
-# import time
-# time.sleep(5)  # Wait for 5 seconds
-# bulb.turn_off()
+if __name__ == '__main__':
+    while True:
+        opt = parse_opt()
+        main(opt)
